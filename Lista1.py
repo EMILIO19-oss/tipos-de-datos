e23eef077f023f69c8988599ba1462de30ed3296
EMILIO19-oss <ibarratavarezemilio@gmail.com>
2025-09-18 08:02:03 -0600

    Create Lista1.py

---
+++ Lista1.py
@@ -0,0 +1,14 @@
+#sirve para poner una lista de personas
+lista =["juan","javier","adrian","jair"]
+print (lista)
+#es para imprimir el segundo nombre de la lista
+print (lista[2])
+#sirve para agregar un elemento a la lista
+lista.append("alexa")
+print (lista)
+lista.insert(1,"javier")
+print (lista)
+#sirve para eliminar un elemento de la lista
+lista.pop(1)
+lista.remove("juan")
+lista.pop(1)
